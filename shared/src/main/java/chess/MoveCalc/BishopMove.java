e460ef914425e77e88caece6860bd7fa82d84175
SVic000 <vivi16@byu.edu>
2026-01-14 23:44:37 -0700

    impl(p0) Tinkering with more of the Interface, Hopefully I'm coding this right. I have no clue how I'm expected to do this in less than 3 hours...

--- shared/src/main/java/chess/MoveCalc/BishopMove.java
+++ shared/src/main/java/chess/MoveCalc/BishopMove.java
@@ -1,4 +1,35 @@
 package chess.MoveCalc;
 
-public class BishopMove {
+import chess.ChessBoard;
+import chess.ChessMove;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Collection;
+import java.util.List;
+
+class BishopMove implements PieceMoves {
+    private final ChessPiece piece;
+    private final ChessBoard board;
+    private final ChessPosition position;
+    private Collection<ChessMove> possibleMovement;
+
+    public BishopMove(ChessPiece piece, ChessBoard board, ChessPosition position) {
+        this.piece = piece;
+        this.board = board;
+        this.position = position;
+    }
+
+    public Collection<ChessMove> getPieceMoves(){
+        int [][] direction =  {{1,-1},{1,1},{-1,1},{-1,-1}};
+        // look into using streams for this!
+        Collection<ChessMove> move1 = this.slide(this.position, this.piece, direction[0], this.board);
+        Collection<ChessMove> move2 = this.slide(this.position, this.piece, direction[1], this.board);
+        Collection<ChessMove> move3 = this.slide(this.position, this.piece, direction[2], this.board);
+        Collection<ChessMove> move4 = this.slide(this.position, this.piece, direction[3], this.board);
+
+
+        return this.possibleMovement;
+    }
+
 }
--- shared/src/main/java/chess/MoveCalc/PieceMovesInterface.java
+++ shared/src/main/java/chess/MoveCalc/PieceMovesInterface.java
@@ -1,38 +1,48 @@
 package chess.MoveCalc;
-import chess.ChessMove;
-import chess.ChessPiece;
+import chess.*;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
-//
  /*
- public interface PieceMovesCalc {
-    public peicemoves;
-    public default diag() {
-        bishop and queen calls
+ Interface PieceMoves {
+
+    public getPieceMoves ();
+    public default slide(int rowChange, int colChange){
+
     }
-    public default plus() [
-        rook and queen
-    }
- }
   */
-//
 
-public class PieceMovesInterface {
-    private List<ChessMove> possiblemoves;
-    private ChessPiece piece;
+interface PieceMoves {
 
-    public PieceMovesInterface(ChessPiece piece){
-        this.piece = piece;
-    }
-    public PieceMovesInterface {
+public Collection<ChessMove> getPieceMoves();
 
+default Collection<ChessMove> slide(ChessPosition myPosition, ChessPiece thisPiece, int[] direction, ChessBoard board) {
+    boolean canSlide = true;
+    int row = myPosition.getRow();
+    int col = myPosition.getColumn();
+    Collection<ChessMove> possibleMoves = new ArrayList<ChessMove>();
 
-    }
-    public PieceMovesInterface {
-        public Collection<ChessMove> pieceMoveSet(ChessBoard board, ChessPosition position){
-            return List.of();
+    while(canSlide) {
+        canSlide = false;
+        ChessPosition whereMove = new ChessPosition(row + direction[0], col + direction[1]);
+        if(whereMove.getRow() > 7 || whereMove.getRow() < 0 || whereMove.getColumn() > 7 || whereMove.getColumn() <0) { // out of grid
+            break;
         }
-    } // talk to a TA about this Like logistics
-}
+        ChessPiece boardPiece = board.getPiece(whereMove);
+        if(boardPiece == null) { // it's empty! add it and keep going
+            possibleMoves.add(new ChessMove(new ChessPosition(row,col), whereMove, null));
+            row += direction[0];
+            col += direction[1]; // keep chugging forward
+            canSlide = true;
+        } else if(boardPiece.getTeamColor() == thisPiece.getTeamColor()) { // same team, stop iterating
+            break;
+        } else { // not on team, add to moves but then break!
+            possibleMoves.add(new ChessMove(new ChessPosition(row,col), whereMove, null));
+            break;
+        }
+    }
+    return possibleMoves;
+} }
