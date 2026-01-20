7a7550417169ebea3611d2f045d3fa5cf7a6acac
SVic000 <vivi16@byu.edu>
2026-01-20 14:13:27 -0700

    impl(p0) Got white to work, now need to put in more thought on the first move (find a way to check the piece right in front of where the piece is)

--- shared/src/main/java/chess/MoveCalculator/PawnMoveCalc.java
+++ shared/src/main/java/chess/MoveCalculator/PawnMoveCalc.java
@@ -24,16 +24,17 @@ public class PawnMoveCalc implements PieceMovesCalc {
 
     private boolean checkCorner(ChessBoard board, int[] corner) {
         if(corner[1] < 1 || corner[1] > 8) {
-            return true; // out of the board, skip this one
+            return false; // out of the board, skip this one
         }
         ChessPiece boardPiece = board.getPiece(new ChessPosition(corner[0], corner[1]));
-        return switch (boardPiece.getTeamColor()) {
-            case null ->
-                // checking forward movement, if column == your current column, you can move there if not you can't
-                    position.getColumn() == corner[1];
-            case WHITE -> color != ChessGame.TeamColor.WHITE;
-            case BLACK -> color != ChessGame.TeamColor.BLACK;
-        };
+        try {
+            return switch (boardPiece.getTeamColor()) {
+                case WHITE -> color != ChessGame.TeamColor.WHITE && corner[1] != position.getColumn();
+                case BLACK -> color != ChessGame.TeamColor.BLACK && corner[1] != position.getColumn();
+            };
+        } catch(Exception e) {
+            return corner[1] == position.getColumn();
+        }
     }
 
     @Override
@@ -41,31 +42,71 @@ public class PawnMoveCalc implements PieceMovesCalc {
         int whereTooCol = position.getColumn();
         ChessPiece.PieceType[] promotable = piece.promotions();
         ChessMove whereMove;
-        int idol = 0;
 
         if (color == ChessGame.TeamColor.WHITE) { // go up
             // if where you're going is 8, promote (context of pos)
             // check directions [0,1], if != null for [1,1],[-1,1]
             // if position.row = 2, add [0,2] as possible move
             if (position.getRow() == 2) { // haven't moved
-                int[][] corners = {{4,position.getColumn()},{3,position.getColumn()},{3,position.getColumn()-1},{3,position.getColumn()+1}};
-                for(int[] ints : corners ) {
-                    if(checkCorner(board, ints)) {
-                        whereMove = new ChessMove(position, new ChessPosition(ints[0],ints[1]), null);
+                int[][] corners = {{4, position.getColumn()}, {3, position.getColumn()}, {3, position.getColumn() - 1}, {3, position.getColumn() + 1}};
+                for (int[] ints : corners) {
+                    if (checkCorner(board, ints)) {
+                        whereMove = new ChessMove(position, new ChessPosition(ints[0], ints[1]), null);
                         possibleMoves.add(whereMove);
                     }
                 }
             } else if (position.getRow() == 7) { // ready to promote // any moves, add promote to it
-                idol +=1;
-            } else { // somewhere in the middle
-                idol +=1;
+                int[][] corners = {{8,position.getColumn()},{8,position.getColumn()+1},{8,position.getColumn()-1}};
+                for (int[] ints : corners) {
+                    if(checkCorner(board,ints)) {
+                        for(ChessPiece.PieceType type : promotable) {
+                            whereMove = new ChessMove(position, new ChessPosition(ints[0],ints[1]), type);
+                            possibleMoves.add(whereMove);
+                        }
+                    }
+                }
+            }  else { // somewhere in the middle
+                    int[][] corners = {{position.getRow()+1, position.getColumn()}, {position.getRow()+1, position.getColumn()+1}, {position.getRow()+1, position.getColumn()-1}};
+                    for (int[] ints : corners) {
+                        if (checkCorner(board, ints)) {
+                            whereMove = new ChessMove(position, new ChessPosition(ints[0], ints[1]), null);
+                            possibleMoves.add(whereMove);
+                        }
+                    }
             }
-        } else {
+        }
+         else {
             // go down
             // if where you're going is 0, promote
             // check directions [0,-1], if != null for [1,-1],[-1,-1] add
             // if position.row = 6, add [0,-2] as possible move
-            idol +=1;
+            if (position.getRow() == 7) { // haven't moved
+                int[][] corners = {{5, position.getColumn()}, {6, position.getColumn()}, {6, position.getColumn() - 1}, {6, position.getColumn() + 1}};
+                for (int[] ints : corners) {
+                    if (checkCorner(board, ints)) {
+                        whereMove = new ChessMove(position, new ChessPosition(ints[0], ints[1]), null);
+                        possibleMoves.add(whereMove);
+                    }
+                }
+            } else if (position.getRow() == 2) { // ready to promote // any moves, add promote to it
+                int[][] corners = {{1,position.getColumn()},{1,position.getColumn()+1},{1,position.getColumn()-1}};
+                for (int[] ints : corners) {
+                    if(checkCorner(board,ints)) {
+                        for(ChessPiece.PieceType type : promotable) {
+                            whereMove = new ChessMove(position, new ChessPosition(ints[0],ints[1]), type);
+                            possibleMoves.add(whereMove);
+                        }
+                    }
+                }
+            }  else { // somewhere in the middle
+                int[][] corners = {{position.getRow()-1, position.getColumn()}, {position.getRow()-1, position.getColumn()+1}, {position.getRow()-1, position.getColumn()-1}};
+                for (int[] ints : corners) {
+                    if (checkCorner(board, ints)) {
+                        whereMove = new ChessMove(position, new ChessPosition(ints[0], ints[1]), null);
+                        possibleMoves.add(whereMove);
+                    }
+                }
+            }
         }
         return possibleMoves;
     }
