a7a0bd9896a04602678e0b616590772f97719cb0
SVic000 <vivi16@byu.edu>
2026-02-24 20:37:36 -0700

    p3 Finished MemoryUserDAO and UserDAO

--- server/src/main/java/dataaccess/TempStorage/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/TempStorage/MemoryUserDAO.java
@@ -1,35 +1,57 @@
 package dataaccess.TempStorage;
 
+import dataaccess.DataAccessException;
 import dataaccess.UserDAO;
 import model.UserData;
 
+import javax.xml.crypto.Data;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MemoryUserDAO implements UserDAO {
     Map<String, UserData> userStorage = new HashMap<>();
+
     @Override
     public void clear() {
         userStorage = new HashMap<>();
     }
 
     @Override
-    public void updateUser(UserData user) {
-
+    public void updateUser(UserData user) throws DataAccessException{
+        if(userStorage.containsKey(user.username())) {
+            userStorage.put(user.username(),user);
+            return;
+        }
+        throw new DataAccessException("User not found");
     }
 
     @Override
-    public void createUser(UserData user) {
-
+    public void createUser(UserData user) throws DataAccessException{
+        if(!userStorage.containsKey(user.username())) {
+            userStorage.put(user.username(),user);
+            return;
+        }
+        throw new DataAccessException("User Already Exists");
     }
 
     @Override
-    public void deleteUser(String userName) {
-
+    public void deleteUser(UserData user) throws DataAccessException{
+       if(userStorage.containsKey(user.username())) {
+           if(userStorage.get(user.username()).equals(user)) {
+               userStorage.remove(user.username());
+               return;
+           }
+           throw new DataAccessException("User info doesn't match given username");
+       }
+       throw new DataAccessException("User was not found");
     }
 
     @Override
-    public UserData getUser(String userName) {
-        return null;
+    public UserData getUser(String userName) throws DataAccessException{
+        if(userStorage.containsKey(userName)) {
+            return userStorage.get(userName);
+        }
+        throw new DataAccessException("User wasn't found");
     }
 }
--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -2,10 +2,12 @@ package dataaccess;
 
 import model.UserData;
 
+import javax.xml.crypto.Data;
+
 public interface UserDAO {
     void clear();
-    void updateUser(UserData user);
-    void createUser(UserData user);
-    void deleteUser(String userName);
-    UserData getUser(String userName);
+    void updateUser(UserData user) throws DataAccessException;
+    void createUser(UserData user) throws DataAccessException;
+    void deleteUser(UserData user) throws DataAccessException;
+    UserData getUser(String userName) throws DataAccessException;
 }
