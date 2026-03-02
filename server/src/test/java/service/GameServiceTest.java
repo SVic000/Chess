11b94a59e37779ffebae487dd9f3ac07656f08b1
SVic000 <vivi16@byu.edu>
2026-03-02 00:54:01 -0700

    p3 setting up for tests, I'm too lazy to write them right now

--- server/src/test/java/service/GameServiceTest.java
+++ server/src/test/java/service/GameServiceTest.java
@@ -1,6 +1,39 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+import dataaccess.storage.MemoryAuthDAO;
+import dataaccess.storage.MemoryGameDAO;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GameServiceTest {
-    // each method needs 1 positive and 1 negative test
-    // methods creategame, joingame, listgame
+    static final GameDAO GAME_STORAGE = new MemoryGameDAO();
+    static final AuthDAO AUTH_STORAGE = new MemoryAuthDAO();
+
+    static final GameService SERVICE = new GameService(AUTH_STORAGE,GAME_STORAGE);
+
+    @BeforeEach
+    void clear() {
+        GAME_STORAGE.clear();
+        AUTH_STORAGE.clear();
+    }
+
+    @Test
+    void createGameSuccess() {}
+
+    @Test
+    void createNoGivenGameName() {}
+
+    @Test
+    void joinGameSuccess() {}
+
+    @Test
+    void joinGameColorTaken(){}
+
+    @Test
+    void listGameSuccess() {}
 }
--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -1,8 +1,43 @@
 package service;
 
-public class UserServiceTest {
-    // 1 pos 1 neg
-    // registeruser, login, logout
+import dataaccess.AuthDAO;
+import dataaccess.UserDAO;
+import dataaccess.storage.MemoryAuthDAO;
+import dataaccess.storage.MemoryUserDAO;
 
-    //
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserServiceTest {
+    static final UserDAO USER_STORAGE = new MemoryUserDAO();
+    static final AuthDAO AUTH_STORAGE = new MemoryAuthDAO();
+
+    static final UserService SERVICE = new UserService(USER_STORAGE,AUTH_STORAGE);
+
+    @BeforeEach
+    void clear() {
+        USER_STORAGE.clear();
+        AUTH_STORAGE.clear();
+    }
+
+    // register pos run one, register again but perhaps change password?
+    @Test
+    void registerUserSuccess() {}
+
+    @Test
+    void registerUserFailure() {}
+
+    // login normal, login different password
+    @Test
+    void loginSuccess(){}
+
+    @Test
+    void loginNotExistingUser(){}
+
+
+    // logout normal, maybe no negative test since auth done in Server?
+    @Test
+    void logoutSuccess() {}
 }
