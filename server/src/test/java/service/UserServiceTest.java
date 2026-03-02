c547a9d22875b902acca32b333df21fe0d2be00e
SVic000 <vivi16@byu.edu>
2026-03-02 12:10:17 -0700

    p3 Implemented UserServiceTests

--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -1,12 +1,24 @@
 package service;
 
 import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
 import dataaccess.UserDAO;
 import dataaccess.storage.MemoryAuthDAO;
 import dataaccess.storage.MemoryUserDAO;
 
+import io.javalin.http.BadRequestResponse;
+import io.javalin.http.ForbiddenResponse;
+import io.javalin.http.UnauthorizedResponse;
+import model.AuthData;
+import model.UserData;
+import org.eclipse.jetty.util.log.Log;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import server.handlers.objects.*;
+
+import javax.print.DocFlavor;
+
+import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,20 +36,53 @@ public class UserServiceTest {
 
     // register pos run one, register again but perhaps change password?
     @Test
-    void registerUserSuccess() {}
+    void registerUserSuccess() throws DataAccessException {
+        RegisterRequest test = new RegisterRequest("username","password","email");
+        RegisterResult actual = SERVICE.register(test);
+
+        assertEquals(test.username(), actual.username());
+        assertNotEquals(null,actual.authToken());
+        assertEquals(1,AUTH_STORAGE.getAuthStorage().size());
+    }
 
     @Test
-    void registerUserFailure() {}
+    void registerUserNoUsername() throws DataAccessException {
+        RegisterRequest test = new RegisterRequest(null, "password", "email");
+        assertThrows(BadRequestResponse.class, ()->SERVICE.register(test));
+    }
 
     // login normal, login different password
     @Test
-    void loginSuccess(){}
+    void loginSuccess() throws DataAccessException {
+        LoginRequest test = new LoginRequest("username","password");
+        USER_STORAGE.createUser(new UserData("username","password","email"));
+
+        LoginResult actual = SERVICE.login(test);
+        Collection<AuthData> authStorage = AUTH_STORAGE.getAuthStorage();
+        assertNotNull(AUTH_STORAGE.getAuth(actual.authToken()));
+    }
 
     @Test
-    void loginNotExistingUser(){}
+    void loginNotExistingUser(){
+        LoginRequest test = new LoginRequest("username","password");
+        assertThrows(UnauthorizedResponse.class, ()->SERVICE.login(test));
+    }
 
 
     // logout normal, maybe no negative test since auth done in Server?
     @Test
-    void logoutSuccess() {}
+    void logoutSuccess() throws DataAccessException {
+        AuthData auth = AUTH_STORAGE.createAuth("username");
+        LogoutRequest test = new LogoutRequest(auth.token());
+        SERVICE.logout(test);
+
+        assertEquals(0, AUTH_STORAGE.getAuthStorage().size());
+    }
+
+    @Test
+    void logoutUnauthorized() {
+        LogoutRequest test = new LogoutRequest("");
+        assertThrows(UnauthorizedResponse.class, ()->SERVICE.logout(test));
+    }
+
 }
