7e163d74b2a94bfd253ab7c395c2b8676d1c0faa
SVic000 <vivi16@byu.edu>
2026-03-17 12:13:27 -0600

    p5 Setting up to figure out how to print board to console

---
+++ client/src/main/java/ui/DrawChessBoard.java
@@ -0,0 +1,16 @@
+package ui;
+
+import chess.ChessGame;
+
+public class DrawChessBoard {
+    private final int CHESSBOARD_WIDTH_IN_SQUARES = 8;
+    //private final ChessGame game;
+
+    // white and observer = white orientation
+    // black = black orientation
+
+    // have two creation methods, one with color and one without for observer!
+
+    // padding
+    private final String EMPTY = "  ";
+}
