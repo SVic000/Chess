4482f95b2dc1263984ca6a084e05c1bb1725f821
SVic000 <vivi16@byu.edu>
2026-03-24 12:25:34 -0600

    p5 finished setting up facade

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,6 +1,7 @@
 package client;
 
 import httpobjs.*;
+import io.javalin.http.HttpResponseException;
 
 import java.net.http.HttpRequest;
 
@@ -20,26 +21,36 @@ public class ServerFacade {
     }
 
     public LoginResult login(LoginRequest request) {
-        return null;
+        var buildReq = communicator.buildRequest("POST", "/session", request);
+        var response = communicator.sendRequest(buildReq);
+        return communicator.handleResponse(response, LoginResult.class);
     }
 
     public CreateGameResult createGame(CreateGameRequest request) {
-        return null;
+        var buildReq = communicator.buildRequest("POST","/game",request);
+        var response = communicator.sendRequest(buildReq);
+        return communicator.handleResponse(response, CreateGameResult.class);
     }
 
     public JoinGameResult joinGame(JoinGameRequest request) {
-        return null;
+        var buildReq = communicator.buildRequest("PUT", "/game", request);
+        var response = communicator.sendRequest(buildReq);
+        return communicator.handleResponse(response, JoinGameResult.class);
     }
 
     public ListGameResult listGames(ClearRequest request) {
-        return null;
+        var buildReq = communicator.buildRequest("GET", "/game", request);
+        var response = communicator.sendRequest(buildReq);
+        return communicator.handleResponse(response, ListGameResult.class);
     }
 
-    public boolean logout(LogoutRequest request) {
-        return false;
+    public void logout(LogoutRequest request) {
+        var buildReq = communicator.buildRequest("DELETE", "/session", request);
+        var response = communicator.sendRequest(buildReq);
     }
 
-    public boolean clear(ClearRequest request) {
-        return false;
+    public void clear(ClearRequest request) {
+        var buildReq = communicator.buildRequest("DELETE", "/db", request);
+        var response = communicator.sendRequest(buildReq);
     }
 }
