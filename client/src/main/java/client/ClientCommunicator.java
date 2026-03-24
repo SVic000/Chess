06d820b5ed41a8a0c38cb673fdc6e4183745b5d3
SVic000 <vivi16@byu.edu>
2026-03-24 12:14:01 -0600

    p5 building ClientCommunicator

--- client/src/main/java/client/ClientCommunicator.java
+++ client/src/main/java/client/ClientCommunicator.java
@@ -1,7 +1,68 @@
 package client;
 
-public class ClientCommunicator {
+import com.google.gson.Gson;
+import io.javalin.http.HttpResponseException;
 
-    // actually build http requests so
-    // facade can send them
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ClientCommunicator {
+    private final String SERVER_URL;
+    private final HttpClient CLIENT = HttpClient.newHttpClient();
+
+
+    public ClientCommunicator(String serverUrl) {
+        SERVER_URL = serverUrl;
+    }
+
+    HttpRequest buildRequest(String method, String path, Object body) {
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(SERVER_URL + path))
+                .method(method, makeRequestBody(body));
+        if (body != null) {
+            request.setHeader("Content-Type", "application/json");
+        }
+        return request.build();
+    }
+
+    HttpRequest.BodyPublisher makeRequestBody(Object request) {
+        if (request != null) {
+            return HttpRequest.BodyPublishers.ofString(new Gson().toJson(request));
+        } else {
+            return HttpRequest.BodyPublishers.noBody();
+        }
+    }
+
+    HttpResponse<String> sendRequest(HttpRequest request) throws HttpResponseException {
+        try {
+            return CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception ex) {
+            throw new HttpResponseException(500, ex.getMessage());
+        }
+    }
+
+    <T> T handleResponse(HttpResponse<String> response, Class<T> responseClass) throws HttpResponseException {
+        var status = response.statusCode();
+
+        if (!isSuccessful(status)) {
+            var body = response.body();
+            if (body != null) {
+                throw new HttpResponseException(status, body);
+            }
+
+            throw new HttpResponseException(status, "other failure: " + status);
+        }
+
+        if (responseClass != null) {
+            return new Gson().fromJson(response.body(), responseClass);
+        }
+
+        return null;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
 }
