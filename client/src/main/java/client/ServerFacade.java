72aaca8ab38a8dcdf27f9069c5c2adc0d617579d
SVic000 <vivi16@byu.edu>
2026-03-24 12:14:09 -0600

    p5 starting the build for ServerFacade

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,7 +1,45 @@
 package client;
 
-public class ServerFacade {
+import httpobjs.*;
 
-    // add call to endpoints here
-    // should be light
+import java.net.http.HttpRequest;
+
+public class ServerFacade {
+    private final String SERVERURL;
+    private final ClientCommunicator communicator;
+
+    public ServerFacade(String url) {
+        SERVERURL = url;
+        communicator = new ClientCommunicator(url);
+    }
+
+    public RegisterResult register(RegisterRequest request) {
+        var buildReq = communicator.buildRequest("POST", "/user", request);
+        var response = communicator.sendRequest(buildReq);
+        return communicator.handleResponse(response, RegisterResult.class);
+    }
+
+    public LoginResult login(LoginRequest request) {
+        return null;
+    }
+
+    public CreateGameResult createGame(CreateGameRequest request) {
+        return null;
+    }
+
+    public JoinGameResult joinGame(JoinGameRequest request) {
+        return null;
+    }
+
+    public ListGameResult listGames(ClearRequest request) {
+        return null;
+    }
+
+    public boolean logout(LogoutRequest request) {
+        return false;
+    }
+
+    public boolean clear(ClearRequest request) {
+        return false;
+    }
 }
