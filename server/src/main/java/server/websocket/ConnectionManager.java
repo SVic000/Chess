cb9ef40357148d4d15ac42751a884afd4b4b8208
SVic000 <vivi16@byu.edu>
2026-04-05 20:52:12 -0600

    p6 ConnectionManager Completed

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,16 +1,50 @@
 package server.websocket;
 
+import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.Session;
+import websocket.commands.UserGameCommand;
+import websocket.messages.ServerMessage;
 
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class ConnectionManager {
     public final Map<Integer, Set<Session>> connections = new HashMap<>();
 
-    // this class's only job is to keep track of who's where and when to send a message!
-    // key = gameid
-    // session = active players (including observers)
+    public void add(int gameID, Session session) {
+        Set<Session> current = connections.get(gameID);
+        if(current == null) {
+            current = new HashSet<>();
+        }
+        current.add(session);
+        connections.put(gameID,current);
+    }
+
+    public void remove(int gameID, Session session) {
+        Set<Session> current = connections.get(gameID);
+        if(current == null) {
+            return;
+        }
+        current.remove(session);
+        connections.put(gameID, current);
+    }
+
+    public void broadcast(Session excludeSession, int gameID, ServerMessage notification) throws IOException {
+        String msg = new Gson().toJson(notification);
+        Set<Session> current = connections.get(gameID);
+        if(current == null) {
+            return;
+        }
+        for (Session c : current) {
+            if(c.isOpen()) {
+                if(!c.equals(excludeSession)) {
+                    c.getRemote().sendString(msg);
+                }
+            }
+        }
+    }
 }
 
